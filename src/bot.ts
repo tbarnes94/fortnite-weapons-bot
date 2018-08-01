75e0afbf985879fa151de958600029aecdc4c522
tbarnes94 <thomas.jonathan.barnes@gmail.com>
2018-07-31 23:33:06 -0400

    bot.ts is cleaned up more.  lists out all relevant stats.  only takes in two arguments. updated help.

--- src/bot.ts
+++ src/bot.ts
@@ -51,92 +51,179 @@ function embedAddField(embed: any, key: string | any, val: string | any, statTyp
 	// 	embed.addField(key, key.title.text);
 	// 	embed.addField('url', key.title.url);
 	// } else {
-		return embed.addField(key, val);
+	return embed.addField(key, val);
 	// }
 }
+
+let helpText: string = 
+	`Type **!armory <weapon: *string*> <rarity: *number*>** to display data for a specific weapon.\n
+	----------------------------------------------------------------------------
+	**weapon:**\n
+	        *Assault Rifles:* \`ar, scar, burst, famas, scoped-ar, thermal\`\n
+	        *Sniper Rifles:* \`hunting, bolt, sas\`\n
+	        *Shotguns:* \`tac, pump, heavy\`\n
+	        *Submachine Guns:* \`smg, s-smg, p90\`\n
+	        *Pistols:* \`pistol, s-pistol, revolver, deagle, akimbo\`\n
+	        *Explosive Weapons*: \`gl, rl, guided\`\n
+			*Big Guns*: \`tommy, mini, lmg\`
+	----------------------------------------------------------------------------
+	**rarity**:\n
+	        **1** \`for Common\`\n
+	        **2** \`for Uncommon\`\n
+	        **3** \`for Rare\`\n
+	        **4** \`for Epic\`\n
+			**5** \`for Legendary\`
+	----------------------------------------------------------------------------`;
+	//**statType**: quick, spread, firingRate, range, damage, envDamage, impactDamage, recoil, lootProbability.`;
 
 client.on('message', (msg: any) => {
 	let args: any = msg.content.split(' ');
 	switch (args[0]) {
-		case '/help':
-			const embed1 = new discord.RichEmbed()
-				.setTitle('Commands Help')
-				.setAuthor('MyMICDS BOT', 'https://mymicds.net/assets/logo/logo.svg')
-				/*
-				* Alternatively, use '#00AE86', [0, 174, 134] or an integer number.
-				*/
-				.setColor(0x42F4C2)
-				.setDescription('Hopefully this will help you navigate the bot')
-				.setFooter('This bot was created by Alex Migala using the discord.js library | Powered by MyMICDS.net backend',
-				// tslint:disable-next-line:max-line-length
-				'https://camo.githubusercontent.com/40129aa4640399b5e65cc3c101361a6a0b5d6467/68747470733a2f2f646973636f72642e6a732e6f72672f7374617469632f6c6f676f2e737667')
-				/*
-				* Takes a Date object, defaults to current date.
-				*/
-				.setTimestamp()
-				// .setURL('https://discord.js.org/#/docs/main/indev/class/RichEmbed')
-				.addField('Help',
-					'!help display help')
-
-				.addField('!checksite', 'Checks to see if the MyMICDS.net site is online')
-
-				.addField('!quote', 'Gets a random quote from our database')
-
-				.addField('!lunch', 'Gets the lunch for today')
-
-				.addField('!break', 'Gets the next long weekend')
-
-				.addField('!stats', 'Gets the amount of people registered with the site')
-
-				.addField('!sports', 'Gets the sports scores')
-
-				.addField('!channel', 'Get stats on the channel')
-
-				// .addField('!note', 'Gets the curent notification')
-				.addField('!weather', 'Gets the current weather at MICDS')
-			/*
-			* Inline fields may not display as inline if the thumbnail and/or image is too big.
-			*/
-			// .addField('Inline Field', 'They can also be inline.', true)
-			/*
-			* Blank field, useful to create some space.
-			*/
-			// .addBlankField(true)
-			// .addField('Inline Field 3', 'You can have a maximum of 25 fields.', true);
-			msg.channel.send({ embed1 });
-			break;
 		case '!armory':
 			try {
 				if (args[1] === 'help') {
-					msg.channel.send('Type ***!armory <weapon> <rarity> <statType>*** to display data for a specific weapon.\n\n\
-					**weapon:** assault-rifle-m4, assault-rifle-scar, assault-rifle-burst, etc.\n\n\
-					**rarity**: common, uncommon, rare, epic, legendary\n\n\
-					**statType**: quick, spread, firingRate, range, damage, envDamage, impactDamage, recoil, lootProbability.');
-				}
-				let weapon = require(`../database/weapons/one/${args[1]}-${args[2]}_${d}.json`);
-				let embed: any = new discord.RichEmbed()
-					.setTitle(`${weapon.name.rarity}`)
-					.setAuthor(`${weapon.name.text}`, null, `${weapon.name.url}`)
-					.setThumbnail(`${weapon.image}`)
-					/*
-					* Alternatively, use '#00AE86', [0, 174, 134] or an integer number.
-					*/
-					.setColor(colorMap[weapon.name.rarity])
-					.setDescription('<Description>')
-					.setFooter('Brought to you by Sarthak, Destroyer of Worlds.')
-					/*
-					* Takes a Date object, defaults to current date.
-					*/
-					.setTimestamp();
-				for (let key in weapon[args[3]]) {
-					let val = weapon[args[3]][key];
-					key = key[0].toUpperCase() + key.slice(1, key.length);
-					key = key.split(/(?=[A-Z])/).join(' ');
-					// console.log(key);
-					embed = embedAddField(embed, key, val, args[3]);
-				}
+					msg.channel.send(helpText);
+				} else {
+					// -- Weapon Shorthand Parser --
+					switch (args[1]) {
+						case 'ar':
+							args[1] = 'assault-rifle-m4';
+							break;
+						case 'scar':
+							args[1] = 'assault-rifle-scar';
+							break;
+						case 'burst':
+							args[1] = 'assault-rifle-burst';
+							break;
+						case 'famas':
+							args[1] = 'assault-rifle-bullpup-burst';
+							break;
+						case 'scoped-ar':
+							args[1] = 'assault-rifle-scoped';
+							break;
+						case 'bolt':
+							args[1] = 'bolt-action-sniper-rifle';
+							break;
+						case 'p90':
+							args[1] = 'compact-gun';
+							break;
+						case 'tommy':
+							args[1] = 'drum-gun';
+							break;
+						case 'drum':
+							args[1] = 'drum-gun';
+							break;
+						case 'akimbo':
+							args[1] = 'dual-pistol';
+							break;
+						case 'gl':
+							args[1] = 'grenade-launcher';
+							break;
+						case 'guided':
+							args[1] = 'guided-missile';
+							break;
+						case 'missile':
+							args[1] = 'guided-missile';
+							break;
+						case 'deagle':
+							args[1] = 'hand-cannon';
+							break;
+						case 'hunting':
+							args[1] = 'hunting-rifle';
+							break;
+						case 'lmg':
+							args[1] = 'light-machine-gun';
+							break;
+						case 'mini':
+							args[1] = 'minigun';
+							break;
+						case 'rl':
+							args[1] = 'rocket-launcher';
+							break;
+						case 'rocket':
+							args[1] = 'rocket-launcher';
+							break;
+						case 'semi-auto':
+							args[1] = 'semi-auto-sniper-rifle';
+							break;
+						case 'sas':
+							args[1] = 'semi-auto-sniper-rifle';
+							break;
+						case 'smg':
+							args[1] = 'submachine-gun';
+							break;
+						case 's-pistol':
+							args[1] = 'suppressed-pistol';
+							break;
+						case 's-smg':
+							args[1] = 'suppressed-smg';
+							break;
+						case 'pump':
+							args[1] = 'pump-shotgun';
+							break;
+						case 'heavy':
+							args[1] = 'heavy-shotgun';
+							break;
+						case 'tac':
+							args[1] = 'tactical-shotgun';
+							break;
+						case 'thermal':
+							args[1] = 'thermal-scoped-assault-rifle';
+							break;
+						default:
+							break;
+					}
 
-				msg.channel.send({ embed });
+					// -- Rarity Shorthand Parser --
+					switch (args[2]) {
+						case '1':
+							args[2] = 'Common';
+							break;
+						case '2':
+							args[2] = 'Uncommon';
+							break;
+						case '3':
+							args[2] = 'Rare';
+							break;
+						case '4':
+							args[2] = 'Epic';
+							break;
+						case '5':
+							args[2] = 'Legendary';
+							break;
+						default:
+							break;
+					}
+
+					let weapon = require(`../database/weapons/one/${args[1]}-${args[2]}_${d}.json`);
+					let keys: string[] = ['quick', 'spread', 'firingRate', 'range', 'damage', 'recoil'];
+					for (let k of keys) {
+						let embed: any = new discord.RichEmbed()
+							.setTitle(`${weapon.name.rarity}`)
+							.setAuthor(`${weapon.name.text}`, null, `${weapon.name.url}`)
+							.setThumbnail(`${weapon.image}`)
+							/*
+							* Alternatively, use '#00AE86', [0, 174, 134] or an integer number.
+							*/
+							.setColor(colorMap[weapon.name.rarity])
+							.setFooter('Brought to you by Sarthak, Destroyer of Worlds.')
+							/*
+							* Takes a Date object, defaults to current date.
+							*/
+							.setTimestamp();
+						// console.log(k);
+						for (let key in weapon[k]) { //weapon[args[3]]) {
+							let val = weapon[k][key]; //let val = weapon[args[3]][key];
+							key = key[0].toUpperCase() + key.slice(1, key.length);
+							key = key.split(/(?=[A-Z])/).join(' ');
+							// console.log(key);
+							// console.log(key);
+							embed.setDescription(k);
+							embed = embedAddField(embed, key, val, k); //embed = embedAddField(embed, key, val, args[3]);
+						}
+						msg.channel.send({ embed });
+					}
+				}
 				break;
 			} catch (err) {
 				msg.channel.send(`**Incorrect Syntax**: try ***!armory help*** for the full description.`);
